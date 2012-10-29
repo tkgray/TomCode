02bbf1bb6aaa07825dddc576c285db4d4682306e
Tom Gray <tom@tkgray.com>
2012-10-29 15:05:12 -0700

    Space Ship Made and Line started

--- TomCode/src/SpaceShip.java
+++ TomCode/src/SpaceShip.java
@@ -8,9 +8,16 @@ import java.applet.*;
 import java.awt.event.*;
 
 public class SpaceShip extends Applet {
+	
+	// instance variables belong to SpaceShip applet
+	private Point clickPoint = null;
+	private final int APPLET_WIDTH = 800, APPLET_HEIGHT = 600;
+	private final int RADIUS = 64;
+	
 	// creates a listener for mouse events
 	public void init() {
 		this.addMouseListener(new MouseL());
+		this.addMouseMotionListener(new MouseL());
 		setBackground (Color.black);
 		setSize(APPLET_WIDTH, APPLET_HEIGHT);
 	}
@@ -20,7 +27,7 @@ public class SpaceShip extends Applet {
 		g.setColor(Color.green);
 		if (clickPoint != null)
 			g.fillOval(clickPoint.x - RADIUS, clickPoint.y - RADIUS,
-					RADIUS * 2, RADIUS * 2);
+					RADIUS * 4, RADIUS * 2);
 	}
 
 	// Sets the point at which to draw the next dot	
@@ -30,23 +37,39 @@ public class SpaceShip extends Applet {
 
 	// MouseL is an inner class, designated as a mouseListener,
 	// which implements the MouseListener Interface	
-	private class MouseL implements MouseListener {
+	private class MouseL implements MouseListener, MouseMotionListener {
 		// code only the method we need	
 		public void mouseClicked(MouseEvent e) {
 			Point pnt = e.getPoint();
 			setPoint(pnt);// inner classes have access to members of enclosing class
-			repaint(); // requests JVM to call paint method
+//			repaint(); // requests JVM to call paint method
+		}
+		
+/*		public void mouseEntered(MouseEvent e) {
+			Point pnt = e.getPoint();
+			setPoint(pnt);// inner classes have access to members of enclosing class
+			repaint(); // requests JVM to call paint method	
+			
+		}
+*/
+		
+		public void mouseMoved(MouseEvent e) {
+			Point pnt = e.getPoint();
+			setPoint(pnt);// inner classes have access to members of enclosing class
+			repaint(); // requests JVM to call paint method	
 		}
 		// list the empty methods from the MouseListener Interface
+		public void mouseDragged(MouseEvent e) {}
 		public void mousePressed(MouseEvent e) {}
 		public void mouseReleased(MouseEvent e) {}
-		public void mouseEntered(MouseEvent e) {}
 		public void mouseExited(MouseEvent e) {}
-	}
 
-	// instance variables belong to EventDemo applet
-	private Point clickPoint = null;
-	private final int APPLET_WIDTH = 800, APPLET_HEIGHT = 600;
-	private final int RADIUS = 64;
+	//	public void mouseClicked(MouseEvent e) {}
+		public void mouseEntered(MouseEvent e) {}
+	
+	}
+	
+
+
 }
 
