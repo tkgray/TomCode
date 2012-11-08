0ff35b099571430e5924f2863692a3dffa0c5968
Tom Gray <tom@tkgray.com>
2012-11-08 11:37:54 -0800

    Trying to get TwoSorts java uploaded

---
+++ TomCode/src/TwoSorts.java
@@ -0,0 +1,48 @@
+/* Name: Thomas Gray
+* Date: 11/14/12
+* Assignment Number: Lesson 9
+* Email: tom.k.gray@gmail.com
+* Work Load:
+* Thoughts:
+*/
+
+import java.awt.Color;
+//import SpaceShip.Butn1Handler;
+//import SpaceShip.Butn2Handler;
+//import SpaceShip.MouseHandler;
+//import SpaceShip.MouseMotionHandler;
+
+
+public class TwoSorts extends Applet {
+
+
+	private final int APPLET_WIDTH = 800, APPLET_HEIGHT = 600; // to make the applett size
+
+	public void init()
+		{
+	
+		setBackground (Color.black);
+		setSize(APPLET_WIDTH, APPLET_HEIGHT);
+	
+	
+	
+	
+/*		addMouseMotionListener (new MouseMotionHandler());
+		addMouseListener (new MouseHandler());
+		setBackground (Color.black);
+		setSize(APPLET_WIDTH, APPLET_HEIGHT);
+		
+		butn1.addActionListener(new Butn1Handler());
+		butn1.setBackground(Color.white);
+		add(butn1);
+		
+		butn2.addActionListener(new Butn2Handler());
+		butn2.setBackground(Color.white);
+		add(butn2);
+		
+
+		sound1 = getAudioClip(getDocumentBase(), "bonk.au");   // loads audio file for laser
+	*/
+		}
+}
+	
